cabe9922340343d96e6e54a97ceffd7702b9536a
JesperNaarttijarvi <30491037+JesperNaarttijarvi@users.noreply.github.com>
2018-05-22 18:20:46 +0200

    Update index.js

--- src/top-header-view/index.js
+++ src/top-header-view/index.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import {
   View,
   Platform,
   StyleSheet,
   StatusBar,
-  Alert,
-  Avatar,
 } from 'react-native';
 
 import {
@@ -14,124 +11,111 @@ import {
   Icon,
   Button,
   SideMenu
-  
 } from 'react-native-elements';
 
 import colorScheme from '../../config/colors';
-import { changeUser, logoutKeycloak, } from '../../actions';
 
 // Class showing the first header. The header should later adjust to other pages.
 export default class TopHeader extends Component {
 
-
   render() {
+    const { title, firstPage, rightIconFunction, backArrowFunction, leftIcons, selectorIcon, modal } = this.props;
 
+    const topPadding = Platform.OS === 'android' && modal ? 30 - StatusBar.currentHeight : 30;
+    // const topPadding = 30;
 
-    const {title, firstPage, rightIconFunction, backArrowFunction, leftIcons, selectorIcon, modal} = this.props;
-
-    const topPadding = Platform.OS === 'android' && modal ? 30-StatusBar.currentHeight : 30;
-    const canBeAccessedEverywhereExceptOnLogin = activeItemKey === 'Login'; //[styles.menuText, styles.unavailableMenuText] : styles.menuText;
-
-    
-
-    return(
+    return (
       <View >
-        <View style={[styles.container, {paddingTop: topPadding}]}>
+        <View style={[styles.container, { paddingTop: topPadding }]}>
           {/* On the landing page on IOS, and all pages on android we want to show a meny icon */}
           {(firstPage) &&
-          <Icon
-            name= 'exit-to-app'
-            color= 'white'
-            size= {40}
-            underlayColor='transparent'
-            title={
-              <View style={styles.textContainer}>
-                  <Text style={[canBeAccessedEverywhereExceptOnLogin]} >Logout</Text>     
-              </View>
-          }
-            onPress={() => this.props.navigation.navigate('DrawerOpen')}
-          />
+            <Icon
+              translateX='90'
+              name='exit-to-app'
+              color={colorScheme.primaryContainerColor}
+              size={50}
+              underlayColor='transparent'
+              onPress={() => this.props.navigation.navigate('DrawerOpen')}
+            />
           }
           {/* But on all other pages on IOS, we want to show a back button  */}
           {(!firstPage) &&
-          <Icon
-            name= 'arrow-back'
-            color= {colorScheme.primaryContainerColor}
-            size= {50}
-            underlayColor='transparent'
-            onPress={() => { 
-              if(!!backArrowFunction) {
-                backArrowFunction();
-              } else {
-                this.props.navigation.goBack();
-              }
-            }}
-          />
+            <Icon
+              name='arrow-back'
+              color={colorScheme.primaryContainerColor}
+              size={50}
+              underlayColor='transparent'
+              onPress={() => {
+                if (!!backArrowFunction) {
+                  backArrowFunction();
+                } else {
+                  this.props.navigation.navigate('StartView');
+                }
+              }}
+            />
           }
           {(!!leftIcons && !!leftIcons.first) &&
             <Icon
-                name={leftIcons.first.name}
-                color={leftIcons.first.color}
-                onPress={leftIcons.first.onPress}
-                size={30}
+              name={leftIcons.first.name}
+              color={leftIcons.first.color}
+              onPress={leftIcons.first.onPress}
+              size={30}
             />}
           {(!!leftIcons && !!leftIcons.second) &&
             <Icon
-                name={leftIcons.second.name}
-                color={leftIcons.second.color}
-                onPress={leftIcons.second.onPress}
-                size={30}
+              name={leftIcons.second.name}
+              color={leftIcons.second.color}
+              onPress={leftIcons.second.onPress}
+              size={30}
             />}
           <Text
-            style= {styles.headerText}
+            style={styles.headerText}
             h4
           >
-          {title}
+            {title}
           </Text>
           {/* Compensate for the two icons to the left of the title */}
           {(!!leftIcons && !!leftIcons.first && !selectorIcon) &&
-            <View style={{width: 30}}/>
+            <View style={{ width: 30 }} />
           }
           {(!!leftIcons && !!leftIcons.second && !selectorIcon) &&
-            <View style={{width: 30}}/>
+            <View style={{ width: 30 }} />
           }
           {(!!leftIcons && !!leftIcons.first && !!leftIcons.second && !!selectorIcon) &&
-            <View style={{width:15}}/>
+            <View style={{ width: 15 }} />
           }
           {!!selectorIcon &&
             <Icon
-            name={selectorIcon.name}
-            color={selectorIcon.color}
-            onPress={selectorIcon.onPress}
-            size={30}
+              name={selectorIcon.name}
+              color={selectorIcon.color}
+              onPress={selectorIcon.onPress}
+              size={30}
             />
           }
           {!!selectorIcon &&
-           <View style={{width:15}}/>
+            <View style={{ width: 15 }} />
           }
           {/* Only render the + icon if we have functionality for it on this view  */}
           {!!rightIconFunction &&
             <Icon
               name='add-circle'
-              size = {50}
+              size={50}
               underlayColor='transparent'
-              color= {colorScheme.primaryContainerColor}
+              color={colorScheme.primaryContainerColor}
               onPress={() => rightIconFunction()}
             />
           }
           {/* If we dont have a function for the + button, we still want to render something of the same size
               otherwise, the title won't be in the right posisition  */}
           {!rightIconFunction &&
-            <View style={{width: 50}}/>
+            <View style={{ width: 50 }} />
           }
         </View>
-    </View>
+      </View>
     );
   }
 }
 //
-
-
 const styles = StyleSheet.create({
   container: {
     paddingTop: 30,
@@ -144,7 +128,7 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   headerText: {
-      color: colorScheme.primaryTextColor,
+    color: colorScheme.primaryTextColor,
   },
   signText: {
     color: colorScheme.primaryTextColor,
